db24d55b5a3467a858a6140b1b26639b0f125af2
Rubarif <rubarif5@gmail.com>
2025-12-23 10:22:41 +0300

    Initial Commit

---
+++ MoviesApp_Team11_Morning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoviesApp_Team11_Morning
+//
+//  Created by Ruba Arif on 03/07/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoviesApp_Team11_Morning/MoviesApp_Team11_MorningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp_Team11_MorningApp.swift
+//  MoviesApp_Team11_Morning
+//
+//  Created by Ruba Arif on 03/07/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp_Team11_MorningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
