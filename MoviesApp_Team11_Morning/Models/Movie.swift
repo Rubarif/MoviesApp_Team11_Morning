a7adf61069be84c640fa474ccd1dbb59a2b1cf6e
Rubarif <rubarif5@gmail.com>
2025-12-28 10:24:30 +0300

    updated files

--- MoviesApp_Team11_Morning/Models/Movie.swift
+++ MoviesApp_Team11_Morning/Models/Movie.swift
@@ -2,15 +2,3 @@
 //  MoviesCenterModel.swift
 //  MoviesApp_Team11_Morning
 //
-/*
-import Foundation
-
-struct Movie: Decodable, Identifiable {
-    let id: String
-    let title: String
-    let duration: String
-    let genre: String
-    let language: String
-    let age: String
-}
-*/
--- MoviesApp_Team11_Morning/Models/Reviews.swift
+++ MoviesApp_Team11_Morning/Models/Reviews.swift
@@ -5,12 +5,4 @@
 //  Created by Ruba Arif on 04/07/1447 AH.
 ///
 ///
-/*
-struct Review: Decodable, Identifiable {
-    let id: String
-    let review_text: String
-    let rate: Int
-    let user_id: String
-    let movie_id: String
-}
-*/
+
